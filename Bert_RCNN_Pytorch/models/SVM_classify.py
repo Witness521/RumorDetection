e17269d4ba5c0a4ce9cc05c695d87da0c37b2a5b
Witness521 <2320815132@qq.com>
2022-03-17 00:16:09 +0800

    给SVM加上K-Fold验证的方法

--- Bert_RCNN_Pytorch/models/SVM_classify.py
+++ Bert_RCNN_Pytorch/models/SVM_classify.py
@@ -11,10 +11,15 @@ class SVM_classify:
         self.x_test = []
         self.y_train = []
         self.y_test = []
+        # 记录acc
+        self.average_acc = 0
+        # 数据集的位置
+        self.dataLocation = '../dataSet/data/Chinese_Rumor_dataset_clean.xls'
 
+    '''正常读取数据集'''
     def readData(self):
         # 读取数据集
-        df = pd.read_excel('../dataSet/data/Chinese_Rumor_dataset_clean.xls')
+        df = pd.read_excel(self.dataLocation)
         # 划分数据集
         # 训练集占总体的80%
         train_data = df.sample(frac=0.8, replace=False, random_state=0, axis=0)
@@ -26,6 +31,31 @@ class SVM_classify:
 
         self.y_train = train_data['label']
         self.y_test = test_data['label']
+
+    '''k折交叉验证'''
+    def kFold_readData(self):
+        # 记录第几个Fold
+        fold = 1
+        # 读取数据集
+        df = pd.read_excel(self.dataLocation)
+        kf = KFold(n_splits=5, shuffle=True)  # , random_state=42
+        # 切割成五个数据集
+        for train_index, test_index in kf.split(df):
+            print('**********Fold ' + str(fold) + '**********')
+            # 根据index获取对应的data(记录一下dataframe格式的数据用iloc切片)
+            train_data, test_data = df.iloc[train_index.tolist(), :], df.iloc[test_index.tolist(), :]
+            # 对train_data和test_data进行shuffle操作
+            train_data = train_data.sample(frac=1, random_state=0)
+            test_data = test_data.sample(frac=1, random_state=0)
+            self.x_train = train_data['post']
+            self.x_test = test_data['post']
+            self.y_train = train_data['label']
+            self.y_test = test_data['label']
+            # 调用SVM方法
+            self.svmClassify()
+            fold += 1
+        print('Test Average Acc:{0:>6.2%}'.format(self.average_acc / 5))
+
 
     # 处理样本
     def svmClassify(self):
@@ -46,8 +76,10 @@ class SVM_classify:
         svc = SVC()
         svc.fit(x_count_train, self.y_train)
         y_predict = svc.predict(x_count_test)
-        print('test acc:', end=' ')
-        print(svc.score(x_count_test, self.y_test))
+        # 准确率
+        acc = svc.score(x_count_test, self.y_test)
+        self.average_acc += acc
+        print('test acc:{0:>6.2%}'.format(acc))
         target_name = ['non-rumor', 'rumor']
         print(classification_report(self.y_test, y_predict, target_names=target_name))
 
@@ -65,5 +97,11 @@ class SVM_classify:
 
 if __name__ == '__main__':
     svmC = SVM_classify()
-    svmC.readData()
-    svmC.svmClassify()
+    isKFold = input("请选择是否使用K折验证(Y/N)")
+    if isKFold in ['Y', 'y']:  # 使用K折验证
+        svmC.kFold_readData()
+    elif isKFold in ['N', 'n']:  # 不使用K折验证
+        svmC.readData()
+        svmC.svmClassify()
+    else:
+        raise Exception("请输入Y或者N:")
---
+++ README.md
@@ -0,0 +1,16 @@
+### 是否使用K折交叉验证在run.py中可选  
+对于模型的选择在run.py中只需更改这三处即可
+![img.png](Bert_RCNN_Pytorch/dataSet/pic/pic1.png)
+
+![img.png](Bert_RCNN_Pytorch/dataSet/pic/pic2.png)
+
+
+
+使用5折交叉验证的结果如下 
+
+1. LSTM的结果:
+![img.png](Bert_RCNN_Pytorch/dataSet/pic/LSTM_result.png)
+2. SVM的结果(直接运行SVM_classify.py即可):
+![img.png](Bert_RCNN_Pytorch/dataSet/pic/SVM_result.png)
+3. TextCNN的结果:
+
